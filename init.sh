aabe4cc7595ecdee08e6cc5896bc162c48378681
K.Kyrylo <kuzmenkoki@gmail.com>
2022-11-27 22:54:14 +0200

    dev7_ans_user

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@
 bash -x tools.inst.sh
 bash -x add.hosts.sh
 bash -x ssh.conf.add.sh
-#bash -x ssh.conf.key.sh
-#bash -x ansible.inst.sh
+bash -x ssh.conf.key.sh
+bash -x ansible.inst.sh
 bash -x git.init.sh
