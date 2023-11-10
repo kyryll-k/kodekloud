1fa7de4bc6af69c1b2cb667a2489b68e61d63346
K.Kyrylo <kuzmenkoki@gmail.com>
2023-11-10 12:26:59 +0200

    restore ans install

--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@ bash -x tools.inst.sh
 bash -x add.hosts.sh
 bash -x ssh.conf.add.sh
 bash -x ssh.conf.key.app.sh
-#bash -x ssh.conf.key.other.sh
-#bash -x ansible.inst.sh
+bash -x ssh.conf.key.other.sh
+bash -x ansible.inst.sh
 bash -x git.init.sh
